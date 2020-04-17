4c3fc735233cc1ce1ed7857ba71ad04f300c7e67
David White <dwhite0627@gmail.com>
2020-04-17 19:23:06 -0400

    Add express app and server in index.js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require("express");
+const server = express();
+const cors = require("cors");
 
-Sing along:
+// Middelware
+server.use(express.json());
+server.use(cors());
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+// Error middleware
+server.use((err, req, res, next) => {
+  console.error(err);
 
-Go code!
-*/
+  if (err) {
+    res
+      .status(500)
+      .json({
+        message: "There was an error performing the required operation",
+      });
+  }
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
