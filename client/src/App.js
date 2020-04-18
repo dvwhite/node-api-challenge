3acb0975c61ff8b193a041b3d9e86de08ac8107e
David White <dwhite0627@gmail.com>
2020-04-17 21:40:50 -0400

    Add Projects component to App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// Component imports
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
